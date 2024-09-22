5e69cfc5f42e1a4a4e52230b8e4adcf662bf837e
Dize dev <axiozweb2022@gmail.com>
2024-09-21 23:35:24 -0500

    se agrego la estructura para botton y el formulario para agregar categoria

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 const wrapper = document.querySelector('.wrapper'),
-    menuBrn = document.querySelector('.menu-btn');
+    backBtn = document.querySelector('.back-btn'),
+    menuBrn = document.querySelector('.menu-btn'),
+    addTaskFrom = document.querySelector('.add-task'),
+    blackDrop = document.querySelector('.black-backdrop'),
+    addButtonTask = document.querySelector('.add-task-button');
 
 const toggleScreen = () => wrapper.classList.toggle('show-category');
 
+const toggleButton = () => addButtonTask.classList.toggle('button--active');
+
+const toggleAddTaskForm = () => {
+    toggleButton();
+    addTaskFrom.classList.toggle('active');
+    blackDrop.classList.toggle('active');
+};
+
+addButtonTask.addEventListener('click', toggleAddTaskForm);
+
 menuBrn.addEventListener('click', toggleScreen);
+
+backBtn.addEventListener('click', toggleScreen);
