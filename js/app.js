0c2bb023dc6bd2ffce9d185877ab0a08f9b88650
Dize dev <axiozweb2022@gmail.com>
2024-09-23 19:23:41 -0500

    mostramos en patalla cuantas tareas tiene cada categoria de forma dinamica

--- js/app.js
+++ js/app.js
@@ -6,7 +6,10 @@ const wrapper = document.querySelector('.wrapper'),
     menuBrn = document.querySelector('.menu-btn'),
     addTaskFrom = document.querySelector('.add-task'),
     blackDrop = document.querySelector('.black-backdrop'),
-    addButtonTask = document.querySelector('.add-task-button');
+    addButtonTask = document.querySelector('.add-task-button'),
+    categoryImg = document.querySelector('#categort-img');
+
+let selectedCategory = categories.at(0);
 
 const toggleScreen = () => wrapper.classList.toggle('show-category');
 
@@ -36,21 +39,52 @@ const filterCategory = category => {
     );
 };
 
-const categoriesContainer = document.querySelector('.categories');
+const categoriesContainer = document.querySelector('.categories'),
+    categoryTitle = document.querySelector('.category--title'),
+    TotalCategoryTask = document.querySelector('.category--task'),
+    totalTask = document.querySelector('.totalTask');
+
+const calculateTotal = () => {
+    const categoryTask = tasks.filter(
+        task =>
+            task.category.toLocaleLowerCase() ===
+            selectedCategory.title.toLocaleLowerCase()
+    );
+
+    TotalCategoryTask.textContent = `${categoryTask.length} Tasks`;
+    totalTask.textContent = tasks.length;
+};
+
+const showSelectedCategory = category => {
+    selectedCategory = category;
+
+    const { title, img } = category;
+
+    categoryTitle.textContent = title;
+    categoryImg.setAttribute('src', `assets/${img}`);
+
+    toggleScreen();
+    calculateTotal();
+};
 
 const renderCategory = () => {
     categoriesContainer.innerHTML = '';
 
-    // Get All the Task of current category
+    /*
+     *Get All the Task of current category
+     */
     categories.map(category => {
         const categoryTask = filterCategory(category);
         const div = document.createElement('div');
 
         div.classList.add('category');
         div.innerHTML = TempleteCategory(category, categoryTask);
+        div.addEventListener('click', () => showSelectedCategory(category));
 
         categoriesContainer.appendChild(div);
     });
 };
+
+calculateTotal();
 
 renderCategory();
